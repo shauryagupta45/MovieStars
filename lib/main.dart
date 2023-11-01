0767f9704c1ff311372beda682bde063f488e841
shauryagupta45 <shauryagupta4501@gmail.com>
2023-11-01 22:30:45 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:movie_stars/utils/routes/routes.dart';
+import 'package:movie_stars/utils/routes/routes_name.dart';
+import 'package:movie_stars/view/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: RoutesName.login,
+      onGenerateRoute: Routes.generateRoute,
+    );
+  }
+}
---
+++ lib/utils/routes/routes.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:movie_stars/utils/routes/routes_name.dart';
+import 'package:movie_stars/view/home_screen.dart';
+
+import '../../view/login_screen.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case RoutesName.home:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => HomeScreen());
+      case RoutesName.login:
+        return MaterialPageRoute(
+            builder: (BuildContext context) => LoginScreen());
+      default:
+        return MaterialPageRoute(builder: (_) {
+          return const Scaffold(
+            body: Center(
+              child: Text('No Route Defined'),
+            ),
+          );
+        });
+    }
+  }
+}
