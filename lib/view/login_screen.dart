ec72d84daa6bfde49250166070c0bed5800861b4
shauryagupta45 <shauryagupta4501@gmail.com>
2023-11-07 22:36:13 +0530

    update

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -19,7 +19,7 @@ class _LoginScreenState extends State<LoginScreen> {
             Navigator.pushNamed(context, RoutesName.home);
             // Navigator.push(context, MaterialPageRoute(builder: (context)=> HomeScreen())) ;
           },
-          child: const Text('Home Screen'),
+          child: const Text('Home Screen '),
         ),
       ),
     );
